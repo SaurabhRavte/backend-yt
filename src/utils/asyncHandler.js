0ff13e9f178316b308b0f2db2e81aa9da71f10de
Saurabh Ravte <saurabh.ravte@gmail.com>
2024-08-30 11:11:43 +0530

    utils and error handling

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () => {}; //Normal
+// const asyncHandler = (func) => () =>{} // function calling funtion;
+// const asyncHandler = (func) => async () =>{} // function calling funtion;
+
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(err.code || 500).json({
+      sucess: false,
+      message: err.message,
+    });
+  }
+};
+*/
