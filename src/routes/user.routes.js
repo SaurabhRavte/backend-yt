49bb193fd2832d45e21bebc30ce859ddf1d4ad12
Saurabh Ravte <saurabh.ravte@gmail.com>
2024-09-04 14:48:32 +0530

    minor fixes in login

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import {
   loginUser,
   registerUser,
-  logoutUSer,
+  logoutUser,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
